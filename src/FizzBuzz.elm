02ffda34948a2aa9bdfb127a3feeb276832fdb18
Brian Balser <brian.balser@gmail.com>
2016-10-14 21:07:22 -0400

    FizzBuzz

---
+++ src/FizzBuzz.elm
@@ -0,0 +1,13 @@
+module FizzBuzz exposing (..)
+
+play : List Int -> List String
+play list =
+  List.map convert list
+
+convert : Int -> String
+convert int =
+  case (rem int 15, rem int 5, rem int 3) of
+    (0, _, _) -> "fizzbuzz"
+    (_, 0, _) -> "buzz"
+    (_, _, 0) -> "fizz"
+    _ -> toString int
